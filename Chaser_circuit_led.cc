7970568f7b0c92bbce5f552504fda4b23898f6eb
wiizzz786 <71535009+wiizzz786@users.noreply.github.com>
2024-05-15 19:49:42 -0700

    Create Chaser_circuit_led.cc

---
+++ Chaser_circuit_led.cc
@@ -0,0 +1,36 @@
+void setup() {
+ pinMode(13, OUTPUT);
+ pinMode(12, OUTPUT);
+ pinMode(11, OUTPUT);
+ pinMode(10, OUTPUT);
+ pinMode(9, OUTPUT);
+}
+void loop() {
+  
+  digitalWrite(13, HIGH);
+  delay(100);
+  digitalWrite(13, LOW);
+  delay(100);
+  
+  digitalWrite(12, HIGH);
+  delay(100);
+  digitalWrite(12, LOW);
+  delay(100);
+  
+  digitalWrite(11, HIGH);
+  delay(100);
+  digitalWrite(11, LOW);
+  delay(100);
+  
+  digitalWrite(10, HIGH);
+  delay(100);
+  digitalWrite(10, LOW);
+  delay(100);
+  
+  digitalWrite(9, HIGH);
+  delay(100);
+  digitalWrite(9, LOW);
+  delay(100);
+}
+  // put your main coe here, to run repeatedly:
+
