158455aac38bff309dfccc55045b26dcde978df1
wiizzz786 <71535009+wiizzz786@users.noreply.github.com>
2024-08-07 15:54:39 -0700

    obstacleavoiding

--- obstacleavoiding.ino
+++ obstacleavoiding.ino
@@ -12,15 +12,15 @@ void setup() {
 
 void forward() {
   //analogWrite(11, 213); line one
-  digitalWrite(11,HIGH);
-  digitalWrite(10,LOW);
+  digitalWrite(11, HIGH);
+  digitalWrite(10, LOW);
   digitalWrite(5, HIGH);
-  digitalWrite(6,LOW);
+  digitalWrite(6, LOW);
 }
 void backward() {
   //backward movement bad
-  digitalWrite(10,HIGH);
-  digitalWrite(11,LOW);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, LOW);
   digitalWrite(5, LOW);
   digitalWrite(6, HIGH);
 }
@@ -45,11 +45,59 @@ void left() {
 
 
 
-void loop(){
-  int c = digitalRead(A1);
-  int l = digitalRead(A2);
-  int r = digitalRead(A0);
-  if (c == )
+void loop() {
+  int c = analogRead(A1);
+  int l = analogRead(A2);
+  int r = analogRead(A0);
+  Serial.begin(9600);
+  Serial.println(c);
+  Serial.println(l);
+  Serial.println(r);
+  if (l == 0 && c == 0 && r == 0) {
+    backward();
+    delay(1000);
+    left();
+    delay(250);
+  }
+  else if ((l == 0 && c == 1 && r == 0)) {
+    backward();
+    delay(1000);
+    right();
+    delay(250);
+  }
+  else if ((l == 0 && c == 0 && r == 1)) {
+    backward();
+    delay(1000);
+    right();
+    delay(250);
+  }
+  else if ((l == 0 && c == 1 && r == 1)) {
+    backward();
+    delay(1000);
+    right();
+    delay(250);
+  }
+  else if ((l == 1 && c == 0 && r == 0)) {
+    backward();
+    delay(1000);
+    left();
+    delay(250);
+  }
+
+  else if ((l == 1 && c == 0 && r == 1)) {
+    backward();
+    delay(1000);
+    left();
+    delay(250);
+  }
+  else if ((l == 1 && c == 1 && r == 0)) {
+    backward();
+    delay(1000);
+    left();
+    delay(250);
+  }
+  else if ((l == 1 && c == 1 && r == 1)) {
+    forward();
+  }
+  
 }
-
-
